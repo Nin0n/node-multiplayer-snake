304d386c0dba7ddb9f23f464dd226789f7d552b0
Ninon Devis <ninon.devis@gmail.com>
2020-01-25 15:46:50 +0100

    Remove semi colon from client side js

--- public/js/controller/game-controller.js
+++ public/js/controller/game-controller.js
@@ -1,8 +1,8 @@
-import ClientConfig from '../config/client-config.js';
-import SampleController from './sample-controller.js';
-import TextToDraw from '../model/text-to-draw.js';
-import CanvasFactory from '../view/canvas-factory.js';
-import GameView from '../view/game-view.js';
+import ClientConfig from '../config/client-config.js'
+import SampleController from './sample-controller.js'
+import TextToDraw from '../model/text-to-draw.js'
+import CanvasFactory from '../view/canvas-factory.js'
+import GameView from '../view/game-view.js'
 
 /**
  * Controls all game logic
@@ -22,67 +22,67 @@ export default class GameController {
                                      this.speedChangeCallback.bind(this),
                                      this.startLengthChangeCallback.bind(this),
                                      this.toggleGridLinesCallback.bind(this)
-                                     );
-        this.audioController = new SampleController();
-        this.players = [];
-        this.food = {};
-        this.textsToDraw = [];
-        this.walls = [];
-        this.score = 0;
+                                     )
+        this.audioController = new SampleController()
+        this.players = []
+        this.food = {}
+        this.textsToDraw = []
+        this.walls = []
+        this.score = 0
     }
 
     connect(io) {
-        this.socket = io();
-        this._initializeSocketIoHandlers();
-        const storedName = localStorage.getItem(ClientConfig.LOCAL_STORAGE.PLAYER_NAME);
-        const storedBase64Image = localStorage.getItem(ClientConfig.LOCAL_STORAGE.PLAYER_IMAGE);
-        this.socket.emit(ClientConfig.IO.OUTGOING.NEW_PLAYER, storedName, storedBase64Image);
+        this.socket = io()
+        this._initializeSocketIoHandlers()
+        const storedName = localStorage.getItem(ClientConfig.LOCAL_STORAGE.PLAYER_NAME)
+        const storedBase64Image = localStorage.getItem(ClientConfig.LOCAL_STORAGE.PLAYER_IMAGE)
+        this.socket.emit(ClientConfig.IO.OUTGOING.NEW_PLAYER, storedName, storedBase64Image)
     }
 
     renderGame() {
-        this.canvasView.clear();
+        this.canvasView.clear()
         for (const foodId of Object.keys(this.food)) {
             if ({}.hasOwnProperty.call(this.food, foodId)) {
-                const food = this.food[foodId];
-                this.canvasView.drawSquare(food.coordinate, food.color);
+                const food = this.food[foodId]
+                this.canvasView.drawSquare(food.coordinate, food.color)
             }
         }
 
-        this.canvasView.drawSquares(this.walls, ClientConfig.WALL_COLOR);
+        this.canvasView.drawSquares(this.walls, ClientConfig.WALL_COLOR)
 
         for (const player of this.players) {
             if (player.segments.length === 0) {
-                continue;
+                continue
             }
             // Flash around where you have just spawned
             if (`/#${this.socket.id}` === player.id &&
                     player.moveCounter <= ClientConfig.TURNS_TO_FLASH_AFTER_SPAWN &&
                     player.moveCounter % 2 === 0) {
-                this.canvasView.drawSquareAround(player.segments[0], ClientConfig.SPAWN_FLASH_COLOR);
+                this.canvasView.drawSquareAround(player.segments[0], ClientConfig.SPAWN_FLASH_COLOR)
             }
 
             if (player.base64Image) {
-                this.canvasView.drawImages(player.segments, player.base64Image);
+                this.canvasView.drawImages(player.segments, player.base64Image)
             } else {
-                this.canvasView.drawSquares(player.segments, player.color);
+                this.canvasView.drawSquares(player.segments, player.color)
             }
         }
 
         for (let i = this.textsToDraw.length - 1; i >= 0; i--) {
-            const textToDraw = this.textsToDraw[i];
+            const textToDraw = this.textsToDraw[i]
             if (textToDraw.counter === ClientConfig.TURNS_TO_SHOW_FOOD_TEXT) {
-                this.textsToDraw.splice(i, 1);
+                this.textsToDraw.splice(i, 1)
             } else {
-                this.canvasView.drawFadingText(textToDraw, ClientConfig.TURNS_TO_SHOW_FOOD_TEXT);
-                textToDraw.incrementCounter();
+                this.canvasView.drawFadingText(textToDraw, ClientConfig.TURNS_TO_SHOW_FOOD_TEXT)
+                textToDraw.incrementCounter()
             }
         }
 
-        const self = this;
+        const self = this
         // Run in a loop
         setTimeout(() => {
-            requestAnimationFrame(self.renderGame.bind(self));
-        }, 1000 / ClientConfig.FPS);
+            requestAnimationFrame(self.renderGame.bind(self))
+        }, 1000 / ClientConfig.FPS)
     }
 
     /*******************
@@ -90,98 +90,98 @@ export default class GameController {
      *******************/
 
     botChangeCallback(option) {
-        this.socket.emit(ClientConfig.IO.OUTGOING.BOT_CHANGE, option);
+        this.socket.emit(ClientConfig.IO.OUTGOING.BOT_CHANGE, option)
     }
 
     foodChangeCallback(option) {
-        this.socket.emit(ClientConfig.IO.OUTGOING.FOOD_CHANGE, option);
+        this.socket.emit(ClientConfig.IO.OUTGOING.FOOD_CHANGE, option)
     }
 
     backgroundImageUploadCallback(image, imageType) {
         if (!(image && imageType)) {
-            this.socket.emit(ClientConfig.IO.OUTGOING.CLEAR_UPLOADED_BACKGROUND_IMAGE);
-            return;
+            this.socket.emit(ClientConfig.IO.OUTGOING.CLEAR_UPLOADED_BACKGROUND_IMAGE)
+            return
         }
-        const resizedBase64Image = this.canvasView.resizeUploadedBackgroundImageAndBase64(image, imageType);
-        this.socket.emit(ClientConfig.IO.OUTGOING.BACKGROUND_IMAGE_UPLOAD, resizedBase64Image);
+        const resizedBase64Image = this.canvasView.resizeUploadedBackgroundImageAndBase64(image, imageType)
+        this.socket.emit(ClientConfig.IO.OUTGOING.BACKGROUND_IMAGE_UPLOAD, resizedBase64Image)
     }
 
     canvasClicked(x, y) {
-        this.socket.emit(ClientConfig.IO.OUTGOING.CANVAS_CLICKED, x, y);
+        this.socket.emit(ClientConfig.IO.OUTGOING.CANVAS_CLICKED, x, y)
     }
 
     // optional resizedBase64Image
     imageUploadCallback(image, imageType, resizedBase64Image) {
         if (!(image && imageType)) {
-            this.socket.emit(ClientConfig.IO.OUTGOING.CLEAR_UPLOADED_IMAGE);
-            localStorage.removeItem(ClientConfig.LOCAL_STORAGE.PLAYER_IMAGE);
-            return;
+            this.socket.emit(ClientConfig.IO.OUTGOING.CLEAR_UPLOADED_IMAGE)
+            localStorage.removeItem(ClientConfig.LOCAL_STORAGE.PLAYER_IMAGE)
+            return
         }
-        let newResizedBase64Image;
+        let newResizedBase64Image
         if (resizedBase64Image) {
-            newResizedBase64Image = resizedBase64Image;
+            newResizedBase64Image = resizedBase64Image
         } else {
-            newResizedBase64Image = this.canvasView.resizeUploadedImageAndBase64(image, imageType);
+            newResizedBase64Image = this.canvasView.resizeUploadedImageAndBase64(image, imageType)
         }
-        this.socket.emit(ClientConfig.IO.OUTGOING.IMAGE_UPLOAD, newResizedBase64Image);
-        localStorage.setItem(ClientConfig.LOCAL_STORAGE.PLAYER_IMAGE, newResizedBase64Image);
+        this.socket.emit(ClientConfig.IO.OUTGOING.IMAGE_UPLOAD, newResizedBase64Image)
+        localStorage.setItem(ClientConfig.LOCAL_STORAGE.PLAYER_IMAGE, newResizedBase64Image)
     }
 
     joinGameCallback() {
-        this.socket.emit(ClientConfig.IO.OUTGOING.JOIN_GAME);
+        this.socket.emit(ClientConfig.IO.OUTGOING.JOIN_GAME)
     }
 
     keyDownCallback(keyCode) {
-        this.socket.emit(ClientConfig.IO.OUTGOING.KEY_DOWN, keyCode);
+        this.socket.emit(ClientConfig.IO.OUTGOING.KEY_DOWN, keyCode)
     }
 
     muteAudioCallback() {
-        this.audioController.toggleMute();
-        this.gameView.setMuteStatus(this.audioController.isMuted);
+        this.audioController.toggleMute()
+        this.gameView.setMuteStatus(this.audioController.isMuted)
     }
 
     playerColorChangeCallback() {
-        this.socket.emit(ClientConfig.IO.OUTGOING.COLOR_CHANGE);
+        this.socket.emit(ClientConfig.IO.OUTGOING.COLOR_CHANGE)
     }
 
     playerNameUpdatedCallback(name) {
-        this.socket.emit(ClientConfig.IO.OUTGOING.NAME_CHANGE, name);
-        localStorage.setItem(ClientConfig.LOCAL_STORAGE.PLAYER_NAME, name);
+        this.socket.emit(ClientConfig.IO.OUTGOING.NAME_CHANGE, name)
+        localStorage.setItem(ClientConfig.LOCAL_STORAGE.PLAYER_NAME, name)
     }
 
     resetScore() {
-        this.score = 0;
+        this.score = 0
         this.audioController.resetScore()
     }
 
     spectateGameCallback() {
-        this.socket.emit(ClientConfig.IO.OUTGOING.SPECTATE_GAME);
+        this.socket.emit(ClientConfig.IO.OUTGOING.SPECTATE_GAME)
     }
 
     speedChangeCallback(option) {
-        this.socket.emit(ClientConfig.IO.OUTGOING.SPEED_CHANGE, option);
+        this.socket.emit(ClientConfig.IO.OUTGOING.SPEED_CHANGE, option)
     }
 
     startLengthChangeCallback(option) {
-        this.socket.emit(ClientConfig.IO.OUTGOING.START_LENGTH_CHANGE, option);
+        this.socket.emit(ClientConfig.IO.OUTGOING.START_LENGTH_CHANGE, option)
     }
 
     toggleGridLinesCallback() {
-        this.canvasView.toggleGridLines();
+        this.canvasView.toggleGridLines()
     }
 
     updateScore(color) {
         switch (color){
             case "blue":
             case "red":
-                this.score += 1;
-                break;
+                this.score += 1
+                break
             case "green":
-                this.score += 5;
-                break;
+                this.score += 5
+                break
             case "yellow":
-                this.score += 25;
-                break;
+                this.score += 25
+                break
         }
         this.audioController.updateScore(this.score)
     }
@@ -193,54 +193,54 @@ export default class GameController {
     _createBoard(board) {
         this.canvasView =
             CanvasFactory.createCanvasView(
-                board.SQUARE_SIZE_IN_PIXELS, board.HORIZONTAL_SQUARES, board.VERTICAL_SQUARES, this.canvasClicked.bind(this));
-        this.canvasView.clear();
-        this.gameView.ready();
-        this.renderGame();
+                board.SQUARE_SIZE_IN_PIXELS, board.HORIZONTAL_SQUARES, board.VERTICAL_SQUARES, this.canvasClicked.bind(this))
+        this.canvasView.clear()
+        this.gameView.ready()
+        this.renderGame()
     }
 
     _handleBackgroundImage(backgroundImage) {
         if (backgroundImage) {
-            this.canvasView.setBackgroundImage(backgroundImage);
+            this.canvasView.setBackgroundImage(backgroundImage)
         } else {
-            this.canvasView.clearBackgroundImage();
+            this.canvasView.clearBackgroundImage()
         }
     }
 
     _handleFoodCollected(text, coordinate, color, isSwap) {
-        this.textsToDraw.unshift(new TextToDraw(text, coordinate, color));
+        this.textsToDraw.unshift(new TextToDraw(text, coordinate, color))
         this.updateScore(color)
     }
 
     _handleNewGameData(gameData) {
-        this.players = gameData.players;
-        this.food = gameData.food;
-        this.walls = gameData.walls;
-        this.gameView.showFoodAmount(Object.keys(gameData.food).length);
-        this.gameView.showSpeed(gameData.speed);
-        this.gameView.showStartLength(gameData.startLength);
-        this.gameView.showNumberOfBots(gameData.numberOfBots);
-        this.gameView.showPlayerStats(gameData.playerStats);
+        this.players = gameData.players
+        this.food = gameData.food
+        this.walls = gameData.walls
+        this.gameView.showFoodAmount(Object.keys(gameData.food).length)
+        this.gameView.showSpeed(gameData.speed)
+        this.gameView.showStartLength(gameData.startLength)
+        this.gameView.showNumberOfBots(gameData.numberOfBots)
+        this.gameView.showPlayerStats(gameData.playerStats)
     }
 
 
     _initializeSocketIoHandlers() {
-        this.socket.on(ClientConfig.IO.INCOMING.NEW_PLAYER_INFO, this.gameView.updatePlayerName);
-        this.socket.on(ClientConfig.IO.INCOMING.BOARD_INFO, this._createBoard.bind(this));
-        this.socket.on(ClientConfig.IO.INCOMING.NEW_STATE, this._handleNewGameData.bind(this));
-        this.socket.on(ClientConfig.IO.INCOMING.NEW_BACKGROUND_IMAGE, this._handleBackgroundImage.bind(this));
-        this.socket.on(ClientConfig.IO.INCOMING.NOTIFICATION.FOOD_COLLECTED, this._handleFoodCollected.bind(this));
-        this.socket.on(ClientConfig.IO.INCOMING.NOTIFICATION.GENERAL, this.gameView.showNotification);
-        this.socket.on(ClientConfig.IO.INCOMING.NOTIFICATION.KILL, this.gameView.showKillMessage.bind(this.gameView));
+        this.socket.on(ClientConfig.IO.INCOMING.NEW_PLAYER_INFO, this.gameView.updatePlayerName)
+        this.socket.on(ClientConfig.IO.INCOMING.BOARD_INFO, this._createBoard.bind(this))
+        this.socket.on(ClientConfig.IO.INCOMING.NEW_STATE, this._handleNewGameData.bind(this))
+        this.socket.on(ClientConfig.IO.INCOMING.NEW_BACKGROUND_IMAGE, this._handleBackgroundImage.bind(this))
+        this.socket.on(ClientConfig.IO.INCOMING.NOTIFICATION.FOOD_COLLECTED, this._handleFoodCollected.bind(this))
+        this.socket.on(ClientConfig.IO.INCOMING.NOTIFICATION.GENERAL, this.gameView.showNotification)
+        this.socket.on(ClientConfig.IO.INCOMING.NOTIFICATION.KILL, this.gameView.showKillMessage.bind(this.gameView))
         this.socket.on(ClientConfig.IO.INCOMING.NOTIFICATION.KILLED_EACH_OTHER,
-            this.gameView.showKilledEachOtherMessage.bind(this.gameView));
+            this.gameView.showKilledEachOtherMessage.bind(this.gameView))
         this.socket.on(ClientConfig.IO.INCOMING.NOTIFICATION.RAN_INTO_WALL,
-            this.gameView.showRanIntoWallMessage.bind(this.gameView));
-        this.socket.on(ClientConfig.IO.INCOMING.NOTIFICATION.SUICIDE, this.gameView.showSuicideMessage.bind(this.gameView));
+            this.gameView.showRanIntoWallMessage.bind(this.gameView))
+        this.socket.on(ClientConfig.IO.INCOMING.NOTIFICATION.SUICIDE, this.gameView.showSuicideMessage.bind(this.gameView))
         this.socket.on(ClientConfig.IO.INCOMING.NOTIFICATION.YOU_DIED,
-            this.audioController.playDeathSound.bind(this.audioController));
-        this.socket.on(ClientConfig.IO.INCOMING.NOTIFICATION.YOU_DIED, this.resetScore.bind(this));
+            this.audioController.playDeathSound.bind(this.audioController))
+        this.socket.on(ClientConfig.IO.INCOMING.NOTIFICATION.YOU_DIED, this.resetScore.bind(this))
         this.socket.on(ClientConfig.IO.INCOMING.NOTIFICATION.YOU_MADE_A_KILL,
-            this.audioController.playKillSound.bind(this.audioController));
+            this.audioController.playKillSound.bind(this.audioController))
     }
 }
--- public/js/controller/sample-controller.js
+++ public/js/controller/sample-controller.js
@@ -1,68 +1,74 @@
-import DomHelper from '../view/dom-helper.js';
+import DomHelper from '../view/dom-helper.js'
 /**
  * Controls all audio logic
  */
 export default class SampleController {
     constructor() {
-        this.sampleCurrentId = -1;
-        this.isMuted = false;
-        this.deathSound = new Audio('assets/death.wav');
-        this.killSound = new Audio('assets/kill.wav');
-        this.scoreNumber = 5;
-        this.samples = [];
-        this.scoresSteps = [];
+        this.sampleCurrentId = -1
+        this.isMuted = false
+        this.deathSound = new Audio('assets/death.wav')
+        this.killSound = new Audio('assets/kill.wav')
+        this.scoreNumber = 5
+        this.samples = []
+        this.scoresSteps = []
         for (let i = 0; i < this.scoreNumber; i++) {
             this.scoresSteps[i] = 10 * (i + 1)
-            this.samples[i] = new Audio(`assets/samples/sample${i+1}.wav`);
-            this.samples[i].loop = true;
+            this.samples[i] = new Audio(`assets/samples/sample${i+1}.wav`)
+            this.samples[i].loop = true
             console.log(`assets/samples/sample${i+1}.wav`)
         }
-        DomHelper.getVolumeSlider().addEventListener('input', this.updateVolume.bind(this));
+        DomHelper.getVolumeSlider().addEventListener('input', this.updateVolume.bind(this))
     }
 
 
     playDeathSound() {
         if (!this.isMuted) {
-            this.deathSound.play();
+            this.deathSound.play()
         }
     }
 
     playKillSound() {
         if (!this.isMuted) {
-            this.killSound.play();
+            this.killSound.play()
         }
     }
 
     playCurrentSample() {
-        this.samples[this.sampleCurrentId].play();
+        this.samples[this.sampleCurrentId].play()
     }
 
     resetCurrentSample() {
         if (this.sampleCurrentId != -1) {
-            this.samples[this.sampleCurrentId].pause();
-            this.samples[this.sampleCurrentId].load();
+            this.samples[this.sampleCurrentId].pause()
+            this.samples[this.sampleCurrentId].load()
         }
     }
 
     resetScore() {
-        this.resetCurrentSample();
-        this.sampleCurrentId = -1;
+        this.resetCurrentSample()
+        this.sampleCurrentId = -1
+    }
+
+    getSampleIdFromScore(score) {
+        const id = ~~(score / 10) - 1
+        return (id < this.scoreNumber) ? id : this.scoreNumber - 1
     }
 
     updateScore(score) {
-        if (this.sampleCurrentId < this.scoreNumber && score >= this.scoresSteps[this.sampleCurrentId+1]) {
-            this.resetCurrentSample();
-            this.sampleCurrentId++;
-            this.playCurrentSample();
+        const scoreSampleId = this.getSampleIdFromScore(score)
+        if(this.sampleCurrentId != scoreSampleId){
+            this.resetCurrentSample()
+            this.sampleCurrentId = scoreSampleId
+            this.playCurrentSample()
         }
     }
 
     updateVolume() {
-        const volume = DomHelper.getVolumeSlider().value;
-        this.samples.forEach(e => e.volume = volume);
+        const volume = DomHelper.getVolumeSlider().value
+        this.samples.forEach(e => e.volume = volume)
     }
 
     toggleMute() {
-        this.isMuted = !this.isMuted;
+        this.isMuted = !this.isMuted
     }
 }
